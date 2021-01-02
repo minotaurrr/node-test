e2ab289e21b02c251a7f2f1a152eab447698ff3e
Min Kim <min.kim030@gmail.com>
2021-01-03 00:58:48 +1100

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res, next) => {
-  res.send('DJDJDJDJ!!!!!!!');
+  res.send('테스트중!!!!!!!');
 });
 
 const server = app.listen(process.env.PORT);
