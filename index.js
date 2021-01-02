054db2daa6869e448d8654673e34efe52e1f8a55
Min Kim <min.kim030@gmail.com>
2021-01-03 01:03:12 +1100

    update release

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res, next) => {
-  res.send('테스트중!!!!!!!');
+  res.send('releasing!!!!!!!');
 });
 
 const server = app.listen(process.env.PORT);
